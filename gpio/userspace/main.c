b9ec51e07bc7ded100c53d5bdffc6bdb48162b54
fudgebread <19294354+fudgebread@users.noreply.github.com>
2018-01-26 18:59:15 +0000

    Update permissions for binary and library. Add GPIO toggle for gpio 26.

--- gpio/userspace/main.c
+++ gpio/userspace/main.c
@@ -6,19 +6,38 @@
  **********************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "include/lib_gpio.h"
 
 int main(int argc, char *argv[])
 {
+	int i, gpio=26;
 	printf("Starting gpio examples...\n");
 	
-	printf("Opening GPIO 23 (pin 16)\n");
+	printf("Opening GPIO %d\n", gpio);
 	
-	if (libGpioOpen(23) != 0)
+	if (libGpioOpen(gpio) != 0)
 		return -1;
 		
-	if (libGpioDirection(23, 0) != 0)
+	if (libGpioDirection(gpio, 0) != 0)
+		return -1;
+		
+	for (i=0; i<10; i++) {
+		printf("Write 1 to gpio %d\n", gpio);
+		if (libGpioBitWrite(gpio, 1) != 0) {
+			return -1;
+		}
+		sleep(1);
+		
+		printf("Write 0 to gpio %d\n", gpio);
+		if (libGpioBitWrite(gpio, 0) != 0) {
+			return -1;
+		}
+		sleep(1);
+	}
+	
+	if (libGpioClose(gpio) != 0)
 		return -1;
 		
 	return 0;
