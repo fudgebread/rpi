f9ac38e54f66b74b6c6f1ce055184a655af862f8
fudgebread <19294354+fudgebread@users.noreply.github.com>
2018-01-27 10:03:29 +0000

    Replace tabs with spaces.

--- gpio/userspace/main.c
+++ gpio/userspace/main.c
@@ -21,70 +21,70 @@
 #define CMD_EXIT "quit"
 
 static const char *commands[] = {CMD_OPEN, CMD_CLOSE, 
-	                             CMD_READ, CMD_WRITE};
+                                 CMD_READ, CMD_WRITE};
 static void printHelp()
 {
-	printf("       [open|close|dir|read|write] <gpio> [in|out] [value]\n");
+    printf("       [open|close|dir|read|write] <gpio> [in|out] [value]\n");
 }
 
 static void* getUserInput(void *args)
 {
-	int gpio, value, i, run=1;
-	char input[80], command[10], dir[5];
-	
-	while (run) {
-		printf("GPIO > ");
-		fgets(input, 80, stdin);
-		
-		if (sscanf(input, "%s %d %s\n", command, &gpio, dir) < 1) {
-			printf("GPIO > invalid command.  Try help\nGPIO > ");
-		}
-		
-		if (strcmp(command, CMD_OPEN) == 0) {
-			libGpioOpen(gpio);
-		}
-		else if (strcmp(command, CMD_CLOSE) == 0) {
-			libGpioClose(gpio);
-		}
-		else if (strcmp(command, CMD_DIR) == 0) {
-			if (strcmp(dir, "in") == 0) {
-				libGpioDirection(gpio, 1);
-			}
-			else if (strcmp(dir, "out") == 0) {
-				libGpioDirection(gpio, 0);
-			}
-			else {
-				printHelp();
-			}
-		}
-		else if (strcmp(command, CMD_READ) == 0) {
-			libGpioBitRead(gpio, &value);
-		}
-		else if (strcmp(command, CMD_WRITE) == 0) {
-			libGpioBitWrite(gpio, atoi(dir));
-		}
-		else if (strcmp(command, CMD_EXIT) == 0) {
-			run = 0;
-		}
-		else {
-			printHelp();
-		}
-	}
-	
-	printf("GPIO > fin!\n");
-	return NULL;
+    int gpio, value, i, run=1;
+    char input[80], command[10], dir[5];
+    
+    while (run) {
+        printf("GPIO > ");
+        fgets(input, 80, stdin);
+        
+        if (sscanf(input, "%s %d %s\n", command, &gpio, dir) < 1) {
+            printf("GPIO > invalid command.  Try help\nGPIO > ");
+        }
+        
+        if (strcmp(command, CMD_OPEN) == 0) {
+            libGpioOpen(gpio);
+        }
+        else if (strcmp(command, CMD_CLOSE) == 0) {
+            libGpioClose(gpio);
+        }
+        else if (strcmp(command, CMD_DIR) == 0) {
+            if (strcmp(dir, "in") == 0) {
+                libGpioDirection(gpio, 1);
+            }
+            else if (strcmp(dir, "out") == 0) {
+                libGpioDirection(gpio, 0);
+            }
+            else {
+                printHelp();
+            }
+        }
+        else if (strcmp(command, CMD_READ) == 0) {
+            libGpioBitRead(gpio, &value);
+        }
+        else if (strcmp(command, CMD_WRITE) == 0) {
+            libGpioBitWrite(gpio, atoi(dir));
+        }
+        else if (strcmp(command, CMD_EXIT) == 0) {
+            run = 0;
+        }
+        else {
+            printHelp();
+        }
+    }
+    
+    printf("GPIO > fin!\n");
+    return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-	int status, startConsole = 0;
-	pthread_t userThread;
-	
-	while ((status = getopt(argc, argv, "c")) != -1) {
+    int status, startConsole = 0;
+    pthread_t userThread;
+    
+    while ((status = getopt(argc, argv, "c")) != -1) {
         switch (status) {
-			case 'c': 
-			startConsole = 1; 
-			break;
+            case 'c': 
+            startConsole = 1; 
+            break;
         default:
             fprintf(stderr, "Usage: %s [-c]\n", argv[0]);
             return -1;
@@ -92,20 +92,20 @@ int main(int argc, char *argv[])
     }
     
     if (startConsole) {
-		printf("Starting gpio console\n");
-		
-		status = pthread_create(&userThread, NULL, getUserInput, NULL);
-		if (status != 0) {
-			fprintf(stderr, "Could not create user input thread (%d)\n", errno);
-			return -1;
-		}
-		
-		status = pthread_join(userThread, NULL);
-		if (status != 0) {
-			fprintf(stderr, "Could not join user input thread (%d)\n", errno);
-			return -1;
-		}
-	}
-		
-	return 0;
+        printf("Starting gpio console\n");
+        
+        status = pthread_create(&userThread, NULL, getUserInput, NULL);
+        if (status != 0) {
+            fprintf(stderr, "Could not create user input thread (%d)\n", errno);
+            return -1;
+        }
+        
+        status = pthread_join(userThread, NULL);
+        if (status != 0) {
+            fprintf(stderr, "Could not join user input thread (%d)\n", errno);
+            return -1;
+        }
+    }
+        
+    return 0;
 }
